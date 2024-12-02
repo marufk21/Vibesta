15492c331491371126f651e85c83eb38a7964132
Maruf77 <marufkhan2002@gmail.com>
2024-12-02 18:29:15 +0530

    MinorChange

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
         e.preventDefault();
         try {
             setLoading(true);
-            const res = await axios.post('//vibesta.onrender.com/vibesta.onrender.com/api/v1/user/login', input, {
+            const res = await axios.post('//vibesta.onrender.com/api/v1/user/login', input, {
                 headers: {
                     'Content-Type': 'application/json'
                 },
